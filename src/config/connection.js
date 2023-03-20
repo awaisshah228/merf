f9443a365b3709a6bd9ddbb3d20f0d451c466ed8
Muhammad Awais Shah <awaisshah228@gmail.com>
2023-03-20 08:36:58 -0700

    done

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const connection = mysql.createPool({
-  connectionLimit : 100,
+  connectionLimit : 50,
   host: process.env.DB_HOST, // Host name for database connection:
   port: process.env.DB_PORT, // Port number for database connection:
   user: process.env.DB_USER, // Database user:
